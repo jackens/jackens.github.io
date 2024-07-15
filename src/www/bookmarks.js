39935f38870d4a2614b93839c1e0b241aaeb44cd
Jackens <jackens@users.noreply.github.com>
2024-07-15 21:36:28 +0200

    nnn

--- src/www/bookmarks.js
+++ src/www/bookmarks.js
@@ -5,13 +5,9 @@ document.title = 'Bookmarks'
 h(document.head,
   ['link', { rel: 'stylesheet', href: '../../node_modules/@picocss/pico/css/pico.min.css' }])
 
-const res = await fetch('../res/bookmarks.md')
-const bookmarksMd = await res.text()
-const /** @type {Partial<Array<import('../nnn/h.js').HArgs>>} */ items =
-  bookmarksMd.trim().split('\n').map(line => {
-    const [, title, href] = line.match(/^- \[(.*?)\]\((.*)\)$/) ?? []
-    return ['li', ['a', { href, target: '_blank' }, title]]
-  })
+const a = /** @returns {import('../../dist/nnn.js').HArgs} */ (
+  /** @type {string} */ text, /** @type {string} */ href
+) => ['li', ['a', { href }, text]]
 
 h(document.body,
   ['style', c({
@@ -19,4 +15,31 @@ h(document.body,
       padding: '20px'
     }
   })],
-  ['ul', ...items])
+  ['ul',
+    a('Pogoda • Katowice', 'https://weather.com/pl-PL/pogoda/godzinowa/l/f2b823199bed31459790ff62f9884bf7e2c6c688716027376a3dab1412c708f6')
+  ],
+  ['ul',
+    a('Chess.com News', 'https://www.chess.com/news'),
+    a('GitHub • Trending', 'https://github.com/trending'),
+    a('GitHub • Trending Go', 'https://github.com/trending/go'),
+    a('GitHub • Trending JavaScript', 'https://github.com/trending/javascript'),
+    a('GitHub • Trending Rust', 'https://github.com/trending/rust'),
+    a('GitHub • Trending TypeScript', 'https://github.com/trending/typescript'),
+    a('GitHub • Trending Zig', 'https://github.com/trending/zig')
+  ],
+  ['ul',
+    a('App Sliced', 'https://appsliced.co/apps/iphone?sort=latest&threshold=all&l=nav'),
+    a('PS Deals', 'https://psdeals.net/pl-store'),
+    a('Apple • Report a Problem', 'https://reportaproblem.apple.com/'),
+    a('YouTube • Andrzej Dragan', 'https://m.youtube.com/results?sp=mAEA&search_query=andrzej+dragan')
+  ],
+  ['ul',
+    a('Lichess', 'https://lichess.org/pl'),
+    a('OpenFm 90s', 'https://www.radio.net/s/openfm90s'),
+    a('BBC News', 'https://www.bbc.com/news/world/europe')
+  ],
+  ['ul',
+    a('Jackens’ Homepage', 'https://jackens.github.io/'),
+    a('GitHub • jackens', 'https://github.com/jackens/'),
+    a('NPM • @jackens/nnn', 'https://www.npmjs.com/package/@jackens/nnn')
+  ])
