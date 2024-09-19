2b49861d005c91af0a5923576e3f0e2d88eb8dd0
Jackens <jackens@users.noreply.github.com>
2024-09-19 19:07:18 +0200

    nnn

--- src/www/bookmarks.js
+++ src/www/bookmarks.js
@@ -3,40 +3,39 @@ import { c, h } from '../../dist/nnn.js'
 document.title = 'Bookmarks'
 
 const a = /** @returns {import('../../dist/nnn.js').HArgs} */ (
-  /** @type {string} */ text, /** @type {string} */ href
-) => ['li', ['a', { href }, text]]
+  /** @type {string} */ text, /** @type {string} */ bref
+) => ['li', ['a', { bref }, text]]
 
 h(document.body,
   ['style', c({
     body: {
       padding: '20px'
-    }
+    },
+    ...Object.fromEntries(Array.from({ length: 3 }, (_, i) => [
+      `@media(min-width:${250 * ++i}px)`, {
+        body: {
+          columns: i
+        }
+      }]))
   })],
   ['ul',
-    a('Pogoda • Katowice', 'https://weather.com/pl-PL/pogoda/godzinowa/l/f2b823199bed31459790ff62f9884bf7e2c6c688716027376a3dab1412c708f6')
-  ],
-  ['ul',
+    a('Apple • Report a Problem', 'https://reportaproblem.apple.com/'),
+    a('BBC News', 'https://www.bbc.com/news/world/europe'),
     a('Chess.com News', 'https://www.chess.com/news'),
-    a('GitHub • Trending', 'https://github.com/trending'),
     a('GitHub • Trending Go', 'https://github.com/trending/go'),
     a('GitHub • Trending JavaScript', 'https://github.com/trending/javascript'),
     a('GitHub • Trending Rust', 'https://github.com/trending/rust'),
     a('GitHub • Trending TypeScript', 'https://github.com/trending/typescript'),
-    a('GitHub • Trending Zig', 'https://github.com/trending/zig')
-  ],
-  ['ul',
-    a('Strava', 'https://www.strava.com'),
-    a('PS Deals', 'https://psdeals.net/pl-store'),
-    a('Apple • Report a Problem', 'https://reportaproblem.apple.com/'),
-    a('YouTube • Andrzej Dragan', 'https://m.youtube.com/results?search_query=andrzej+dragan')
-  ],
-  ['ul',
-    a('Lichess', 'https://lichess.org/pl'),
-    a('OpenFm 90s', 'https://www.radio.net/s/openfm90s'),
-    a('BBC News', 'https://www.bbc.com/news/world/europe')
-  ],
-  ['ul',
-    a('Jackens’ Homepage', 'https://jackens.github.io/'),
+    a('GitHub • Trending Zig', 'https://github.com/trending/zig'),
+    a('GitHub • Trending', 'https://github.com/trending'),
     a('GitHub • jackens', 'https://github.com/jackens/'),
-    a('NPM • @jackens/nnn', 'https://www.npmjs.com/package/@jackens/nnn')
+    a('Jackens’ Homepage', 'https://jackens.github.io/'),
+    a('Lichess', 'https://lichess.org/pl'),
+    a('NPM • @jackens/nnn', 'https://www.npmjs.com/package/@jackens/nnn'),
+    a('OpenFm 90s', 'https://www.radio.net/s/openfm90s'),
+    a('PS Deals', 'https://psdeals.net/pl-store'),
+    a('Pogoda • Katowice',
+      'https://weather.com/pl-PL/pogoda/godzinowa/l/f2b823199bed31459790ff62f9884bf7e2c6c688716027376a3dab1412c708f6'),
+    a('Strava', 'https://www.strava.com'),
+    a('YouTube • Andrzej Dragan', 'https://m.youtube.com/results?search_query=andrzej+dragan')
   ])
